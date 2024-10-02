166a42c2362a35eaa8674c6f7ee95407d9ecd5d2
AKharitonashvili <sandroxari@gmail.com>
2024-10-02 20:49:54 +0400

    feature file upload component

---
+++ src/app/shared/ui/inputs/file-upload/file-upload.component.ts
@@ -0,0 +1,87 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  forwardRef,
+  signal,
+} from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-file-upload',
+  standalone: true,
+  imports: [MatIconModule, MatProgressBarModule],
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => FileUploadComponent),
+      multi: true,
+    },
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FileUploadComponent implements ControlValueAccessor {
+  @Input() requiredFileType: string = 'image/*';
+
+  fileName = signal<string | null>(null);
+  filePreviewUrl = signal<string | ArrayBuffer | null>(null);
+  isDisabled = signal<boolean>(false);
+
+  private onChange = (value: File | null) => {};
+  private onTouched = () => {};
+
+  onFileSelected(event: any): void {
+    if (this.isDisabled()) {
+      return;
+    }
+
+    const file: File = event.target.files[0];
+
+    if (file && this.isImageFile(file)) {
+      this.fileName.set(file.name);
+
+      const reader = new FileReader();
+      reader.onload = () => this.filePreviewUrl.set(reader.result);
+      reader.readAsDataURL(file);
+      this.onChange(file);
+    } else {
+      alert('Please upload an image file.');
+    }
+  }
+
+  private isImageFile(file: File): boolean {
+    return file.type.startsWith('image/');
+  }
+
+  writeValue(value: File | null): void {
+    if (value) {
+      this.fileName.set(value.name);
+      const reader = new FileReader();
+      reader.onload = () => this.filePreviewUrl.set(reader.result);
+      reader.readAsDataURL(value);
+    } else {
+      this.fileName.set(null);
+      this.filePreviewUrl.set(null);
+    }
+  }
+
+  registerOnChange(fn: (value: File | null) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled.set(isDisabled);
+  }
+
+  onTouchedEvent() {
+    this.onTouched();
+  }
+}
